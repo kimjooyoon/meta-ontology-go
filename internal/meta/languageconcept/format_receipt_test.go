3d71dbecb1237c9c4f120270204e75db9053fc9e
kimjooyoon <asketeddy@gmail.com>
2026-08-22 19:24:55 +0900

    ci: request Go 1.27 format receipt

---
+++ internal/meta/languageconcept/format_receipt_test.go
@@ -0,0 +1,22 @@
+package languageconcept
+
+import (
+	"encoding/base64"
+	"go/format"
+	"os"
+	"testing"
+)
+
+func TestGo127FormatReceipt(t *testing.T) {
+	for _, name := range []string{"evaluate.go", "evidence.go", "model.go"} {
+		source, err := os.ReadFile(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		formatted, err := format.Source(source)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("FORMAT_RECEIPT %s %s", name, base64.StdEncoding.EncodeToString(formatted))
+	}
+}
