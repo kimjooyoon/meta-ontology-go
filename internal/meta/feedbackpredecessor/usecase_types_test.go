f9a3e93e7010f157f761a3e75896988558a258aa
kimjooyoon <asketeddy@gmail.com>
2026-08-22 18:30:28 +0900

    meta: apply Go 1.27 use-case format receipt

--- internal/meta/feedbackpredecessor/usecase_types_test.go
+++ internal/meta/feedbackpredecessor/usecase_types_test.go
@@ -6,16 +6,16 @@ type useCaseFile struct {
 }
 
 type useCase struct {
-	ID                         string `json:"id"`
-	Actor                      string `json:"actor"`
-	Trigger                    string `json:"trigger"`
-	Cause                      string `json:"cause"`
-	CandidateState             string `json:"candidate_state"`
-	ExpectedDecision           string `json:"expected_decision"`
-	ExpectedReason             string `json:"expected_reason"`
-	ExpectedReadinessBPS       int    `json:"expected_readiness_bps"`
-	ExpectedAmbiguity          int    `json:"expected_ambiguity"`
-	ExpectedWrites             int    `json:"expected_writes"`
-	ExpectedSatisfied          int    `json:"expected_satisfied_indicators"`
-	OperatorAction             string `json:"operator_action"`
+	ID                   string `json:"id"`
+	Actor                string `json:"actor"`
+	Trigger              string `json:"trigger"`
+	Cause                string `json:"cause"`
+	CandidateState       string `json:"candidate_state"`
+	ExpectedDecision     string `json:"expected_decision"`
+	ExpectedReason       string `json:"expected_reason"`
+	ExpectedReadinessBPS int    `json:"expected_readiness_bps"`
+	ExpectedAmbiguity    int    `json:"expected_ambiguity"`
+	ExpectedWrites       int    `json:"expected_writes"`
+	ExpectedSatisfied    int    `json:"expected_satisfied_indicators"`
+	OperatorAction       string `json:"operator_action"`
 }
