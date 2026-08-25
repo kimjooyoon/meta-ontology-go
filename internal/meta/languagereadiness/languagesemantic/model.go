8908cb3d1972a65a0a7ed4ee7757279a2d096cf5
kimjooyoon <asketeddy@gmail.com>
2026-08-25 15:22:21 +0900

    fix(language): bind test examples to semantic corpus

--- internal/meta/languagereadiness/languagesemantic/model.go
+++ internal/meta/languagereadiness/languagesemantic/model.go
@@ -4,7 +4,7 @@ const (
 	ReportSchema   = "gooo/language-semantic-model/v1"
 	RegistrySchema = "gooo/language-semantic-model-corpus/v1"
 	ConceptID      = "language-semantic-model"
-	FixedTotal     = 22
+	FixedTotal     = 24
 )
 
 type Decision string
