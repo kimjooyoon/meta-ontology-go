5b0fe691733877d9ea4e3b74983e0d6c3a017aa7
kimjooyoon <asketeddy@gmail.com>
2026-08-26 17:51:11 +0900

    refactor(compiler): isolate reachability witness

--- internal/packageruntime/artifactemit/symbolic_value_reachability_analyze.go
+++ internal/packageruntime/artifactemit/symbolic_value_reachability_analyze.go
@@ -65,15 +65,3 @@ func symbolicValueSchemaProfileSupported(schema *InvocationSchema) bool {
 	}
 	return true
 }
-
-func symbolicValueSchemaProvidesReadyWitness(schema *InvocationSchema) bool {
-	if schema == nil || len(schema.Examples) != 1 {
-		return false
-	}
-	expectedInputs := make([]string, len(schema.Properties.Inputs.PrefixItems))
-	for index, item := range schema.Properties.Inputs.PrefixItems {
-		expectedInputs[index] = item.Const
-	}
-	example := schema.Examples[0]
-	return example.Activity == schema.Properties.Activity.Const && slices.Equal(example.Inputs, expectedInputs)
-}
