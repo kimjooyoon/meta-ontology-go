d03e46b95a4ff26f5fab76ffd4d5cde30d2eb5c6
kimjooyoon <asketeddy@gmail.com>
2026-08-28 08:35:13 +0900

    fix: register Workgraph syntax corpus

--- internal/meta/languagereadiness/languagesyntax/model.go
+++ internal/meta/languagereadiness/languagesyntax/model.go
@@ -11,8 +11,8 @@ const (
 	ResolutionLower = "LOWER_RESOLUTION"
 	KindValid       = "VALID"
 	KindInvalid     = "INVALID"
-	totalCases      = 34
-	validCases      = 31
+	totalCases      = 35
+	validCases      = 32
 	invalidCases    = 3
 	FixedTotal      = totalCases
 	invalidDigest   = "sha256:0000000000000000000000000000000000000000000000000000000000000000"
