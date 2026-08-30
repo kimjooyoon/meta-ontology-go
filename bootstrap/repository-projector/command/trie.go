195fe1b1a94f73815d4f7f3fa559a83f6e820750
kimjooyoon <asketeddy@gmail.com>
2026-08-31 06:21:36 +0900

    Remove obsolete projector evidence helper

--- bootstrap/repository-projector/command/trie.go
+++ bootstrap/repository-projector/command/trie.go
@@ -63,12 +63,3 @@ func assignGroup(group []*storedObject, prefix []string, depth int) error {
 	}
 	return nil
 }
-
-func requireBlockingZero(report evidence) error {
-	for _, metric := range report.Indicators {
-		if metric.Blocking && metric.Value > metric.Limit {
-			return fmt.Errorf("blocking indicator %s=%d", metric.ID, metric.Value)
-		}
-	}
-	return nil
-}
