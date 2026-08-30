d00651a8b9fdee0da140c6cf2e7681bd0fedd1f9
kimjooyoon <asketeddy@gmail.com>
2026-08-30 18:14:03 +0900

    Route CLI semantic checks through EntityFields support

--- cmd/gooo/semantic_check.go
+++ cmd/gooo/semantic_check.go
@@ -16,9 +16,31 @@ func semanticCheckIR(file *syntax.File, timeout time.Duration) (semantic.IR, err
 }
 
 func semanticCheckIRWithEntityFieldsSupport(file *syntax.File, timeout time.Duration) (semantic.IR, error) {
-	return semanticCheckIRWithLowerer(file, timeout, func(file *syntax.File) (semantic.IR, error) {
+	return lowerEntityFieldsInspectIRWith(file, timeout, func(file *syntax.File) (semantic.IR, error) {
 		return bidir.LowerContextWithEntityFieldsSupport(context.Background(), file, syntax.EntityFieldsV1Support())
 	})
+}
+
+func lowerEntityFieldsInspectIRWith(file *syntax.File, timeout time.Duration, lower func(*syntax.File) (semantic.IR, error)) (semantic.IR, error) {
+	if timeout <= 0 {
+		return semantic.IR{}, errCommandDeadline
+	}
+	result := make(chan inspectLowerResult, 1)
+	go func() {
+		ir, err := lower(file)
+		if err == nil {
+			err = ir.Validate()
+		}
+		result <- inspectLowerResult{ir: ir, err: err}
+	}()
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+	select {
+	case lowered := <-result:
+		return lowered.ir, lowered.err
+	case <-timer.C:
+		return semantic.IR{}, errCommandDeadline
+	}
 }
 
 func semanticCheckIRWithLowerer(file *syntax.File, timeout time.Duration, lower func(*syntax.File) (semantic.IR, error)) (semantic.IR, error) {
