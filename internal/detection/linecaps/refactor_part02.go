318dc98d17d343ef47300079a89c3e5d94bee9ee
kimjooyoon <asketeddy@gmail.com>
2026-09-06 09:10:46 +0900

    Keep collapse candidate type check

--- internal/detection/linecaps/refactor_part02.go
+++ internal/detection/linecaps/refactor_part02.go
@@ -30,7 +30,9 @@ func CollapseAssignReturn(fset *token.FileSet, node ast.Node, comments []*ast.Co
 		return false
 	}
 	assign := body.List[0].(*ast.AssignStmt)
-	result := body.List[1].(*ast.ReturnStmt)
+	if _, ok := body.List[1].(*ast.ReturnStmt); !ok {
+		return false
+	}
 	for _, comment := range comments {
 		if comment.Pos() >= assign.Pos() && comment.Pos() <= body.Rbrace {
 			return false
