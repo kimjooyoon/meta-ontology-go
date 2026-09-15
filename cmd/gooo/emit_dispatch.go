7d56659bfaffe05eff4e5933f074cf11c019f13a
kimjooyoon <asketeddy@gmail.com>
2026-09-15 23:08:44 +0900

    fix(cli): route replay commands through extension dispatch

--- cmd/gooo/emit_dispatch.go
+++ cmd/gooo/emit_dispatch.go
@@ -7,6 +7,10 @@ import (
 
 func runExtensionCommand(args []string, stdout, stderr io.Writer) int {
 	switch args[0] {
+	case "compare":
+		return runCompareReplay(args[1:], OSFileReader{}, stdout, stderr)
+	case "propose-repair":
+		return runProposeRepair(args[1:], OSFileReader{}, stdout, stderr)
 	case "emit":
 		return runEmit(args[1:], stdout, stderr)
 	case "certify":
